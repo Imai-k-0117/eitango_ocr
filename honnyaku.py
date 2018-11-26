786b765aa9ded2c08c75325aa9357ae3a67ec32f
Imai-k-0117 <kazu0117011@gmail.com>
2018-11-26 16:42:48 +0900

    first commit

---
+++ honnyaku.py
@@ -0,0 +1,42 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 26 16:13:37 2018
+
+@author: KAZUKI
+"""
+
+# -*- coding: utf-8 -*-
+import requests, uuid, json
+
+target_text = """----------"""
+
+base_url = '--------------------------------'
+path = '----------------------'
+params = "&to=ja"
+constructed_url = base_url + path + params
+
+headers = {
+    'Ocp-Apim-Subscription-Key': '-------------------',
+    'Content-type': 'application/json',
+    'X-ClientTraceId': str(uuid.uuid4())
+}
+
+body = [{
+    'text' : target_text
+}]
+
+request = requests.post(constructed_url, headers=headers, json=body)
+response = request.json()
+
+print(response[0]['translations'][0]['text'])
+
+
+
+
+
+
+
+
+
+
+
---
+++ tanngo.py
@@ -0,0 +1,32 @@
+﻿# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 26 14:30:35 2018
+
+@author: KAZUKI
+"""
+import re
+from collections import Counter
+
+# 00 テキストの取得
+target_text = """----------"""
+
+# 01 文章を単語に分ける
+# 複数の区切り文字を指定するため re.split を使う
+words = re.split(r'\s|\,|\.|\(|\d+\)', target_text.lower())
+
+# 02 集計する
+counter = Counter(words)
+print(len(counter))
+
+# 03 表示する
+# Counter#most_common を使って出現頻度の降順に csv 形式で表示する
+for word, count in counter.most_common():
+    if len(word) > 0:
+        print("%s,%d" % (word, count))
+        
+        
+
+
+
+
+
